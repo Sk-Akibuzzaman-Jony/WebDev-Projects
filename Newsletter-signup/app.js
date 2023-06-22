016d55777242a13e128aef2b0d38b9e447e16ae0
Sk Akibuzzaman Jony <108605211+Sk-Akibuzzaman-Jony@users.noreply.github.com>
2023-06-22 21:34:11 +0530

    Add files via upload

---
+++ Newsletter-signup/app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var request = require('superagent');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+var mailchimpInstance   = 'us21',
+    listUniqueId        = 'd724a27356',
+    mailchimpApiKey     = '2c5c9440680c48ddfd952ef6d713e1a6-us21';
+
+
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+"/signup.html");
+});
+
+
+app.post('/', function (req, res) {
+    request
+        .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+        .send({
+          'email_address': req.body.email,
+          'status': 'subscribed',
+          'merge_fields': {
+            'FNAME': req.body.fName,
+            'LNAME': req.body.lName
+          }
+        })
+            .end(function(err, response) {
+              if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+                res.sendFile(__dirname+'/success.html');
+              } else {
+                res.sendFile(__dirname+'/failure.html'); 
+              }
+
+          });
+});
+
+app.post("/failure",function(req, res){
+    res.redirect("/");
+})
+
+
+app.listen(3000, function () {
+  console.log('server started on port 3000');
+});
+
+
+  //api key  --   2c5c9440680c48ddfd952ef6d713e1a6-us21
+//   audience ID = d724a27356
